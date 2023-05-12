32118ad6cb5ed412b94462d817fecdd7ead7289b
Victor9i <paulovgomes23@gmail.com>
2023-05-12 00:25:33 -0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Styles from './styles/App.module.css';
+
+let frases = [
+  "foi cancelado(a) por não fazer nada.😌",
+    "foi cancelado(a) por contar muita piada ruim.😔",
+    "foi cancelado(a) por ser insuportável.😤",
+    "foi cancelado(a) por ser insensível 😠",
+    "foi cancelado(a) por ser atraente demais.😘",
+    "foi cancelado(a) por por se considerar incancelável.🤓",
+    "foi cancelado(a) por ser trouxa.😶",
+    "foi cancelado(a) por ser uma delícia.😋",
+    "foi cancelado(a) por ser corno.👹",
+    "foi cancelado(a) por ser uma grande gostoso(a).😍",
+    "foi cancelado(a) por ser padrãozinho.🤭",
+    "foi cancelado(a) por ser inútil.🗑",
+    "foi cancelado(a) por debochar demais.😤",
+    "foi cancelado(a) por ser boy lixo.🤮"       
+];
+
+function gerarFrase() {
+  let frase = frases[
+    Math.floor(Math.random() *
+      frases.length)
+  ]
+  console.log(frase);
+  document.querySelector('#frase-gerada').textContent=frase;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className={Styles.main}>
+        <div className={Styles.card}>
+          <h1 className={Styles.titulo}>Gerador de Cancelamento</h1>
+          
+          <p>
+            <button onClick={gerarFrase}  className={Styles.botao}>Gerar meu cancelamento</button>
+          </p>
+          <p id="frase-gerada" className={Styles.resultado}></p>
+        </div>
+      </main>
+    </>
+
   );
 }
 
